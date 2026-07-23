7f8e9ce32a383d05e378ebb8ea5413e366ff0dc4
mishrayash18 <yashbmishra@gmail.com>
2026-07-23 16:39:00 +0530

    solved problem 66, plus one on leetcode

---
+++ python/leetcode problems/plusone_66.py
@@ -0,0 +1,13 @@
+def plusOne(digits: list[int]) -> list[int]:
+    n = len(digits)
+
+    for i in range(n - 1, -1, -1):
+        if digits[i] < 9:
+            digits[i] += 1
+            return digits
+        else:
+            digits[i] = 0
+
+    return [1] + digits
+
+print(plusOne([9,1,0]))
